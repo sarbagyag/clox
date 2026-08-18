137ceecdbc4a8973dc9c867c43942f874f6f9bf3
sarbagya <sarbagyaghoshrestha@gmail.com>
2026-08-18 10:37:12 +0545

    feat: initial setups in clox

---
+++ src/chunk.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "chunk.h"
+#include "memory.h"
+
+void initChunk(Chunk *chunk)
+{
+    chunk->count = 0;
+    chunk->capacity = 0;
+    chunk->code = NULL;
+}
+
+void writeChunk(Chunk *chunk, uint8_t byte, int line)
+{
+
+    if (chunk->capacity < chunk->count + 1)
+    {
+        int oldCapacity = chunkCapacity;
+        chunk->capacity = GROW_CAPACITY(oldCapacity);
+        chunk->code = GROW_ARRAY(uint8_t, chunk->code, oldCapacity, chunk->capacity);
+    }
+
+    chunk->code[chunk->count] = byte;
+    chunk->count++;
+}
+
+void freeChunk(Chunk *chunk)
+{
+    FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    initChunk(chunk);
+}
